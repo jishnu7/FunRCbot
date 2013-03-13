5434bf2b3fdb5f1bcbe24637e6e705719ed9c06d
Jishnu Mohan <jishnu7@gmail.com>
2013-03-13 08:56:11 +0530

    avoid bot's nick selecting for making tea

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ HOST, PORT = 'irc.freenode.net', 6667
 # Bot's name and channel
 CHANNEL, NICK = '#channel', 'nick_name'
 # Other bots/accounts to skip for commands like !maketea
-BOTS = ['@ChanServ']
+BOTS = ['@ChanServ', NICK]
